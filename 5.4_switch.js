194f467476102dbcb89bd6c33965a5a228169672
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 21:27:50 +0600

    File  edit fast time

--- 5.4_switch.js
+++ 5.4_switch.js
@@ -1,1 +1,15 @@
-lz
+let person;
+
+switch (person) {
+    case 'Sakib':
+        console.log("Doesn't number 1 all-rounder");
+        // break;
+        case 'Tamim':
+            console.log("Best Batsman in Bangladesh");
+            // break;
+        case 'Mushfiq':
+            console.log("Mr. Dependable");
+            // break;
+    default:
+        console.log("Please enter a valid name");
+}
