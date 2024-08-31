03d42a55c121994d5cdcbf061e0ca3e836b2a146
Atikur Rhaman <designletter999@gmail.com>
2024-09-01 01:08:46 +0600

    add file

---
+++ 6.4_foreach.js
@@ -0,0 +1,10 @@
+let arr = ['Saidur', 'Rhaman', 'Atikur', 'Rhamn', 'Atik', 'Emon', 'Tamim', 'Setu', 'Shakib'];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+arr.forEach(function(el) {
+    console.log(el);
+});
