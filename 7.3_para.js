489d032de5f05dd55139502623805d50aae15574
Atikur Rhaman <designletter99@gmail.com>
2024-09-02 23:24:09 +0600

    add file & edit code

--- 7.3_para.js
+++ 7.3_para.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+
 let a = 30;
-let b =20;
+let b = 20;
 
 function add(num1, num2) {
     let result = num1 + num2;
@@ -11,9 +11,9 @@ add(a, b);
 add(30, 30);
 
 let c = 30;
-=======
-let a = 30;
-let b =20;
+
+// let a = 30;
+// let b =20;
 
 function add(num1, num2) {
     let result = num1 + num2;
@@ -22,4 +22,3 @@ function add(num1, num2) {
 
 add(a, b);
 add(30, 30);
->>>>>>> 64570d6579d7056fb08fb1ebd506f0f2725654f9
