ea8c364ec1cdafa2f15b4836fc4bc63aec8795fc
Atikur Rhaman <designletter99@gmail.com>
2024-09-03 21:40:09 +0600

    add file

---
+++ 8.5_this.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'Atikur',
+    sayHello: function() {
+        console.log('Hello ' + this.name);
+    },
+};
+
+person.sayHello();
+
+// method -> Object
+// function -> global, window
+
