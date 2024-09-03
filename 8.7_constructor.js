0f19a941bb131792eed9dd7f308b6b51dd34fc89
Atikur Rhaman <designletter99@gmail.com>
2024-09-04 01:49:13 +0600

    edit code

--- 8.7_constructor.js
+++ 8.7_constructor.js
@@ -8,7 +8,3 @@ function Person(name, age) {
 
 let person1 = new Person('Atikur', 14);
 let person2 = new Person('Rhaman', 15);
-
-console.log(person2);git remote add origin https://github.com/atik9019/Serach.git
-git branch -M main
-git push -u origin main
