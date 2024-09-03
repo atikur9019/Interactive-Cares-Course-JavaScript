b4d57ff5d1068c26d98ea3bd7ae9c72635292a97
Atikur Rhaman <designletter99@gmail.com>
2024-09-03 23:38:36 +0600

    edit code-1

--- 8.5_this.js
+++ 8.5_this.js
@@ -1,12 +1,42 @@
-let person = {
-    name: 'Atikur',
-    sayHello: function() {
-        console.log('Hello ' + this.name);
-    },
-};
+// let person = {
+//     // name: 'Atikur',
+//     greeting: 'Hello',
+//     name: ['Atikur', 'Sakib', 'Mushifk'],
+//     showName: function() {
+//         // console.log('Hello ' + this.name);
+//         this.name.forEach(function(el) {
+//             console.log(el);
+//         });
+//     },
+// };
 
-person.sayHello();
+// person.showName();
 
 // method -> Object
 // function -> global, window
 
+
+
+
+//JavaScript Objects
+let newSymb = Symbol('key1');
+const mobileModel = {
+    //key: value
+    brand: 'Samsung',
+    model: 's24 Ultra',
+    processor: 'Sanpdragon Gen 3',
+    camera: ['200Mp', '12Mp', '12Mp'],
+    hasZoomCamera: true,
+    'selfie camera Mp': 12,
+    [newSymb] : 'MyKey1',
+};
+
+
+mobileModel.model = 's25 Ultra'; // Update key value
+Object.freeze(mobileModel); // Freeze Object
+mobileModel.model = 's26 Ultra';
+mobileModel.hasOwnProperty('camera') // Search key
+console.log(mobileModel.model);
+
+console.log(mobileModel['selfie camera Mp']);
+console.log(mobileModel[newSymb]);
